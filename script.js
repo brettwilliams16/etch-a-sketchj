d7cb2894f8767e6acdf28c098fcd01beec8e1b82
brettwilliams16 <brett3901@gmail.com>
2023-01-10 11:36:54 -0500

    finished the hovering affect in script.js

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const container = document.querySelector(".container");
+const newGrid = document.querySelector(".new-grid");
+let squares = null;
 
-makeGrid();
+makeGrid(10);
 
+function clearBoard() {
+    let squaresToRemove = document.querySelectorAll(".gridSquare");
+    squaresToRemove.forEach(square => {
+        square.parentNode.removeChild(square);
+    })
+}
 
-
+function generateGrid(){
+    clearBoard();
+    let dim = prompt("Please enter the new dimensions:");
+    if(dim > 100){
+        dim = prompt("Please enter a number less than 100:");
+    }
+    makeGrid(dim);
+}
 function hovered(e) {
     this.classList.toggle("hovered");
 }
-function makeGrid() {
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
-            let gridPiece = document.createElement("div");
-            gridPiece.classList.add("gridSquare");
-            container.appendChild(gridPiece);
+function makeGrid(newSize) {
+    container.style.setProperty(`grid-template-columns`, `repeat(${newSize}, 1fr)`);
+    container.style.setProperty(`grid-template-rows`, `repeat(${newSize}, 1fr)`);
+    for(let i = 0; i < newSize; i++){
+        for(let j = 0; j < newSize; j++){
+            let gridSquare = document.createElement("div");
+            gridSquare.classList.add("gridSquare");
+            container.appendChild(gridSquare);
         }
     }
+    squares = null;
+    squares = document.querySelectorAll(".gridSquare");
+    squares.forEach(square =>{
+        square.addEventListener("mouseover", hovered);
+    })
 }
 
-const squares = document.querySelectorAll(".gridSquare");
-
-squares.forEach(square =>{
-    square.addEventListener("mouseover", hovered);
-})
+newGrid.addEventListener("click", generateGrid);
